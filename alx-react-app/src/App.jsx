3114a8e0ae1279c8bbb5d0b48a69933128637c7c
Adedokun Adesoye <michealdavidjeffrey@gmail.com>
2026-01-09 15:54:05 -0800

    add uerprofile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,12 +2,14 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 function App() {
   return (
     <div>
       <Header />
       <MainContent />
       <WelcomeMessage />
+      <UserProfile name="Alice" age="25" bio="Love hiking and photography" />
       <Footer />
     </div>
     
