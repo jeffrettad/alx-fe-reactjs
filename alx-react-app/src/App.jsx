1a06e69f1e9fcd72abbe32f86b4773feaa8c15e4
Adedokun Adesoye <michealdavidjeffrey@gmail.com>
2026-01-09 15:26:57 -0800

    add header main and footer

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,15 @@
 import WelcomeMessage from './components/WelcomeMessage';
-
+import Header from './components/Header';
+import Main from './components/MainContent';
+import Footer from './components/Footer';
 function App() {
   return (
     <div>
+      <Header />
+      <Main />
       <WelcomeMessage />
+      <Footer />
+
     </div>
   );
 }
---
+++ alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function Main(){
+    return(
+        
+<main>
+  <p>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+
+    );
+
+}
+export default Main;
