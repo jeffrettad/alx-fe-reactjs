68843b627252074bce31e2b8a8984a123fc0facb
Adedokun Adesoye <michealdavidjeffrey@gmail.com>
2026-01-16 00:18:10 -0800

    add alx-react-app-new

---
+++ alx-react-app-new/src/components/Header.jsx
@@ -0,0 +1,19 @@
+
+function Header(){
+    return(
+       <header style={{
+        backgroundColor: 'navy',
+        color: 'white',
+        textAlign: 'center',
+        padding: '15px'
+      }}>
+        <h1>My Favorite Cities</h1>
+        </header>  
+    )
+
+}
+
+<header>
+  <h1>My Favorite Cities</h1>
+</header>
+export default Header;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+function MainContent() {
+  return (
+    <main  style={{
+        padding: '20px',
+        backgroundColor: '#f2f2f2'
+      }}>
+      <p style={{ fontSize: '18px' }}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
