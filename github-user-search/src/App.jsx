394dd01b6818fdedd727a01e20c7ea77c420c6fa
Adedokun Adesoye <michealdavidjeffrey@gmail.com>
2026-02-01 04:00:51 -0800

    add the changes

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,23 +1,15 @@
 import React from "react";
 import Search from "./components/Search";
 
-
 function App() {
   return (
-   <div className="flex justify-center items-center min-h-screen bg-gray-50">
-  <h1 className="text-3xl font-bold text-blue-600">
-    GitHub User Search
-  </h1>
-   <Search />
-</div>
-
-
-      
-    
+    <div className="flex flex-col items-center min-h-screen bg-gray-50 p-6">
+      <h1 className="text-3xl font-bold text-blue-600 text-center mb-6">
+        GitHub User Search
+      </h1>
+      <Search />
+    </div>
   );
 }
-
-
-
 
 export default App;
