e280162699d9ead71fb84954f651e6202bb47190
Adedokun Adesoye <michealdavidjeffrey@gmail.com>
2026-02-14 05:07:47 -0800

    add readme

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,13 @@
+import HomePage from "./components/HomePage";
+
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
