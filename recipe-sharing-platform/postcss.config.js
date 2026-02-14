7b494d42394b3c0ef496bb4ac8bfc5ab7dfb7a76
Adedokun Adesoye <michealdavidjeffrey@gmail.com>
2026-02-14 05:31:07 -0800

    add tailwind css

--- recipe-sharing-platform/postcss.config.js
+++ recipe-sharing-platform/postcss.config.js
@@ -1,8 +1,6 @@
-import postcss from "postcss";
-
-export default {
+module.exports = {
   plugins: {
-    "@tailwindcss/postcss": {},
-    
+    tailwindcss: {},
+    autoprefixer: {},
   },
-}
+};
--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,7 +1,8 @@
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
