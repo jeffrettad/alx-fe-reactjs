aaed6f6cd87295b06f393b0fc20f65e337facb4b
Adedokun Adesoye <michealdavidjeffrey@gmail.com>
2026-02-20 17:14:44 -0800

    add formikForm.js

---
+++ form-handling-react/src/components/formikForm.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+function FormikForm() {
+
+  const initialValues = {
+    username: "",
+    email: "",
+    password: ""
+  };
+
+  const validationSchema = Yup.object({
+
+    username: Yup.string()
+      .required("Username is required"),
+
+    email: Yup.string()
+      .email("Invalid email")
+      .required("Email is required"),
+
+    password: Yup.string()
+      .min(6, "Password must be at least 6 characters")
+      .required("Password is required")
+  });
+
+  const handleSubmit = (values, { resetForm }) => {
+
+    console.log(values);
+
+    alert("Formik registration successful");
+
+    resetForm();
+  };
+
+  return React.createElement(
+
+    Formik,
+    {
+      initialValues: initialValues,
+      validationSchema: validationSchema,
+      onSubmit: handleSubmit
+    },
+
+    React.createElement(
+      Form,
+      null,
+
+      React.createElement("h2", null, "Formik Registration Form"),
+
+      React.createElement(Field, {
+        type: "text",
+        name: "username",
+        placeholder: "Username"
+      }),
+
+      React.createElement(ErrorMessage, {
+        name: "username",
+        component: "p"
+      }),
+
+      React.createElement(Field, {
+        type: "email",
+        name: "email",
+        placeholder: "Email"
+      }),
+
+      React.createElement(ErrorMessage, {
+        name: "email",
+        component: "p"
+      }),
+
+      React.createElement(Field, {
+        type: "password",
+        name: "password",
+        placeholder: "Password"
+      }),
+
+      React.createElement(ErrorMessage, {
+        name: "password",
+        component: "p"
+      }),
+
+      React.createElement(
+        "button",
+        { type: "submit" },
+        "Register"
+      )
+
+    )
+
+  );
+}
+
+export default FormikForm;
